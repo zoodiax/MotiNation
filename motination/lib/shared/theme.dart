85ad0c49d7def99bd46c4306526c06c59decc565
Sponi-o <70881986+Sponi-o@users.noreply.github.com>
2021-03-10 11:12:13 +0100

    Add files via upload

--- motination/lib/shared/theme.dart
+++ motination/lib/shared/theme.dart
@@ -35,6 +35,12 @@ getTheme(){
                             color: Colors.black,
                             fontSize: 22,
                             fontWeight: FontWeight.w400)),
+
+                            headline6: GoogleFonts.spartan(
+                        textStyle: TextStyle(
+                            color: bgColor,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600)),
       bodyText1: GoogleFonts.spartan(
                         textStyle: TextStyle(
                             color: Colors.black,
